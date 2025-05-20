43169a10ac763a5e4ee8ce5e15bc493536ea50e2
Vaishnavii621 <136267681+Vaishnavii621@users.noreply.github.com>
2025-05-20 12:06:39 +0530

    Update axios.js

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD ? window.location.origin : "http://localhost:5000",
+  baseURL: import.meta.env.PROD
+    ? `${window.location.origin}/api/v1`
+    : "http://localhost:5000/api/v1",
   withCredentials: true, // if using cookies for auth
 });
 
